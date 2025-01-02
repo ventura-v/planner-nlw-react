577bad71b09424d7fe80fe3620bd12b2d82109a5
Vinicius Ventura <vinicius__20@hotmail.com>
2025-01-01 21:36:45 -0300

    refactoring: creating Button global component

---
+++ src/components/button.tsx
@@ -0,0 +1,33 @@
+import { ComponentProps, ReactNode } from "react";
+import { tv, VariantProps } from "tailwind-variants";
+
+const buttonVariants = tv({
+    base: "rounded-lg px-3 py-2 font-medium flex items-center gap-2",
+
+    variants: {
+        variant: {
+            primary: "bg-lime-300 text-lime-950 hover:bg-lime-400",
+            secondary: "bg-zinc-800 text-zinc-200  hover:bg-zinc-700"
+        }
+    },
+
+    defaultVariants: {
+        variant: "primary"
+    }
+})
+
+interface ButtonProps extends ComponentProps<'button'>, VariantProps<typeof buttonVariants>{
+    children: ReactNode,
+}
+
+export function Button({ children, variant, ...props }: ButtonProps) {
+    return (
+        <button 
+            className={buttonVariants({ variant })}
+            {...props}
+        >
+            {children}
+        </button>
+    )
+}
+
