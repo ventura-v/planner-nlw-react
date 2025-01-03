a67d899eb429467b08622699c7657f84f6b7fd3e
Vinicius Ventura <vinicius__20@hotmail.com>
2025-01-03 19:15:55 -0300

    feat: connection with db

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,5 +1,8 @@
 import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { useParams } from "react-router-dom";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
 
 interface CreateActivityModalProps {
     closeCreateActivityModal: () => void;
@@ -8,6 +11,23 @@ interface CreateActivityModalProps {
 export function CreateActivityModal({
     closeCreateActivityModal,
 }: CreateActivityModalProps) {
+    const { tripId } = useParams();
+
+    const createActivity = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const data = new FormData(event.currentTarget)
+        const title = data.get("title")?.toString()
+        const occurs_at = data.get("occurs_at")?.toString()
+        
+        await api.post(`/trips/${tripId}/activities`, {
+            title,
+            occurs_at,
+        })
+
+        window.document.location.reload()
+    }
+
     return (
         <div className="fixed inset-0 bg-zinc-950 flex items-center justify-center">
             <div className="w-[540px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -23,7 +43,7 @@ export function CreateActivityModal({
                     </p>
                 </div>
 
-                <form className="space-y-3">
+                <form onSubmit={createActivity} className="space-y-3">
                     <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
                         <Tag className="text-zinc-400 size-5" />
                         <input 
--- src/pages/trip-details/guests.tsx
+++ src/pages/trip-details/guests.tsx
@@ -1,33 +1,45 @@
-import { CircleDashed, UserCog } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { CheckCircle2, CircleDashed, UserCog } from "lucide-react";
 import { Button } from "../../components/button";
 
+interface Participant {
+    "id": string,
+    "name": string | null,
+	"email": string,
+	"is_confirmed": boolean
+}
+
 export function Guests() {
+    const { tripId } = useParams();
+    const [participants, setParticipants] = useState<Participant[]>([])
+    
+    useEffect(() => {
+        api.get(`/trips/${tripId}/participants`).then(response => { setParticipants(response.data.participants) })
+    }, [tripId])
+
     return (
         <div className="space-y-6">
             <h2 className="font-semibold text-xl">Convidados</h2>
             <div className="space-y-5">
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">
-                            Vinicius Ventura
-                        </span>
-                        <span className="block text-xs text-zinc-400 truncate">
-                            vinicius.ventura@ventech.com.br
-                        </span>
+                {participants.map(( participant, index ) => (
+                    <div key={participant.id} className="flex items-center justify-between gap-4">
+                        <div className="space-y-1.5">
+                            <span className="block font-medium text-zinc-100">
+                                {participant.name ?? `Convidado ${index}`}
+                            </span>
+                            <span className="block text-xs text-zinc-400 truncate">
+                                {participant.email}
+                            </span>
+                        </div>
+                        {participant.is_confirmed ? (
+                            <CheckCircle2 className="text-lime-400 size-5 shrink-0" />
+                        ) : (
+                            <CircleDashed className="text-zinc-400 size-5 shrink-0" />
+                        )}
                     </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
-                <div className="flex items-center justify-between gap-4">
-                    <div className="space-y-1.5">
-                        <span className="block font-medium text-zinc-100">
-                            Dev Ventura
-                        </span>
-                        <span className="block text-xs text-zinc-400 truncate">
-                            dev.ventura@venth.com.br
-                        </span>
-                    </div>
-                    <CircleDashed className="text-zinc-400 size-5 shrink-0" />
-                </div>
+                    ))}
             </div>
             <Button variant="secondary" size="full">
                 <UserCog className="size-5"/>
